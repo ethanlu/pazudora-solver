3366e575f7854f501424eae9c46e58119aa2e4ef
Ethan Lu <fang.lu@gmail.com>
2017-05-20 16:00:46 +0000

    removed extraneous test from setup

--- setup.py
+++ setup.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python
 
-import sys
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-
-
-class PyTestCommand(TestCommand):
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 setup(
     name="pazudorasolver",
@@ -45,10 +27,6 @@ setup(
     install_requires=[
         "termcolor==1.1"
     ],
-    tests_require=[
-        "pytest"
-    ],
-    cmdclass={
-        'test': PyTestCommand
-    }
+    tests_require=[],
+    cmdclass={}
 )
